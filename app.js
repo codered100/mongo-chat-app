8d9b2f40c6f0978eed5e82f6e8cc8bbd2dd5b25b
jonathan <fischer_casey@outlook.com>
2017-11-29 14:36:43 -0800

    9th

--- app.js
+++ app.js
@@ -89,6 +89,10 @@ io.on('connection', function (socket) {
                     if (err) { console.warn(err.message); }
                     else { console.log("chat message inserted into db: " + msg); }
                 });
+                collection.insert({ content: msg }, function (err, o) { //JON ADDED
+                    if (err) { console.warn(err.message); }
+                    else { console.log("chat message inserted into db: " + msg); }
+                });
             }
         });
 
