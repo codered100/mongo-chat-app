324eeaee825f54988f980a0074d3fd2262a16ffe
jonathan <fischer_casey@outlook.com>
2017-12-04 11:05:50 -0800

    removed close

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ io.on('connection', function (socket) {
                 collection.insert({ content: msg }, function (err, o) {
                     if (err) { console.warn(err.message); }
                     else { console.log("chat message inserted into db: " + msg); }
-                    db.close();                
+                  //  db.close();                
                     
                 });
               //  collection.insert({ content: msg }, function (err, o) { //JON ADDED
@@ -112,7 +112,7 @@ io.on('connection', function (socket) {
                     collection.insert({ content: "OPLOGASD" }, function (err, o) {
                         if (err) { console.warn(err.message); }
                         else { console.log("chat message inserted into db: " + msg); }
-                        db.close();                
+                     //   db.close();                
                         
                     });
                 }
