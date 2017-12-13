2d0065f4b9e01add7618bbbdd386f14f3f5eedc0
jonathan <fischer_casey@outlook.com>
2017-12-13 10:42:25 -0800

    asd

--- app.js
+++ app.js
@@ -82,13 +82,14 @@ oplog.tail().then(() => {
  oplog.on('insert', doc => {
   //  console.log(doc.o.temperature);
     //var data = JSON.stringify({ temperature: doc.o.temperature, humidity: doc.o.humidity, pressure: doc.o.pressure, id: doc.o.id, station: doc.o.station});
-    var temp = doc.o._wperm;
+    var temp =  doc.o._wperm;
     
    // console.log(temp);
 
    // var role = temp.split("/'", 1)
    // console.log("what is the role?");
    // console.log(role);
+   console.log(temp);
     if (temp.includes("DEMOLOCATION")){
         var mesg = new Message(JSON.stringify({ uuidRaw: doc.o.uuidRaw, _id: doc.o._id, _p_pd: doc.o._p_pd,_p_uuid: doc.o._p_uuid, _p_tag: doc.o._p_tag, _p_pd_coord: doc.o._p_pd_coord, major: doc.o.major, minor: doc.o.minor, txPwr: doc.o.txPwr, rssi: doc.o.rssi, role: doc.o._wperm, _created_at: doc.o._created_at, _updated_at: doc.o._updated_at}));
     console.log("1");
